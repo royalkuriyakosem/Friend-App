27d6d52c140f2ee7841b1031ac5d9f26138dfe34
royalkuriyakosem <royalkuriyakosem@gmail.com>
2024-06-29 14:34:33 +0530

    second commit

--- app/src/main/java/com/example/friendsapp/MainActivity.java
+++ app/src/main/java/com/example/friendsapp/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.friendsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText ed1,ed2,ed3,ed4;
+    AppCompatButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ed1=(EditText) findViewById(R.id.name);
+        ed2=(EditText) findViewById(R.id.friendName);
+        ed3=(EditText) findViewById(R.id.friendNickName);
+        ed4=(EditText) findViewById(R.id.describe);
+        b1=(AppCompatButton) findViewById(R.id.submit);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=ed1.getText().toString();
+                String friendName=ed2.getText().toString();
+                String friendNickName=ed3.getText().toString();
+                String submit=ed4.getText().toString();
+
+                Toast.makeText(getApplicationContext(),name, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
